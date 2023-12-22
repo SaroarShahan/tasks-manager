f86a8c40ba3547c61f8b4704f7e5ba5210d7fa6f
SaroarShahan <saroar.shahan@gmail.com>
2023-12-22 14:59:51 +0600

    Add index.app file

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+dotenv.config();
+
+const app = express();
+
+const errorHandler = require('./src/middleware/errorHandler');
+const notFound = require('./src/middleware/notFound');
+const tasksRoutes = require('./src/routes/tasksRoutes');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/v1/tasks', tasksRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const _PORT = process.env.PORT || 4000;
+
+// Database connection
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB)
+  .then(() => console.log('Database connection successful!'))
+  .catch((err) => console.log(err));
+
+// Server
+app.listen(_PORT, () => {
+  console.log(`Server running at http://localhost:${_PORT}`);
+});
